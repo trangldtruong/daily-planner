8cb87b56de837c39ba8c687ea438efef9c291b4f
Trang Truong <trangldtruong@gmail.com>
2023-09-26 04:36:18 -0400

    a screenshot added, function to compare the hour added

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Daily Planner
 
 ## Description 
+This application allows user to plan their work day. It shows the current time in the header and 
 
 ## Usage 
 
 ## Visual
+a screenshot of the application can be found at C:\Users\trang\bootcamp\daily-planner\Screenshot 2023-09-25 205558.png
+
+## License
+MIT 
 
 ## Contribution 
+the author is open to any contributions to the codebase. 
--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-//var past = document.getElementsById("past")
-//var present = document.getElementsById("present")
-//var future = document.getElementsById("future")
-var saveBtn = document.getElementsByClassName("saveBtn")
-let currentDate = document.getElementById('currentDay')
-currentDate.textContent = dayjs().format('dddd, MMMM D, YYYY h:mm A');
-console.log(saveBtn);
-saveBtn 
+var saveBtn = document.querySelectorAll('.saveBtn')
+let date = document.getElementById('currentDay')
+let time = document.getElementById('currentTime')
+date.textContent = dayjs().format('MMMM D, YYYY');
+time.textContent = dayjs().format('h:mm A')
+
+var hour = dayjs('H');
+
+$('.time-block').each(function() {
+  var currentTime = parseInt(hour);
+  console.log(currentTime);
+  var timeBlock = parseInt($(this).attr("id").split("-")[1]);
+if (timeBlock < currentTime) {
+  $(this).addClass("past");
+} else if (timeBlock === currentTime) {
+  $(this).addClass("present");
+} else if (timeBlock > currentTime) {
+  $(this).addClass("future");
+}
+});
 
 
-$(function () {
-  //var save = addEventListener('click', saveBtn)
+$(".saveBtn").click (function (event) {
+  event.preventDefault();
+  localStorage.getItem();
+})
+//$(function )
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -23,14 +38,12 @@ $(function () {
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // past, present, and future classes? 
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   
-  // TODO: Add code to display the current date in the header of the page.
   
    
-});
+
