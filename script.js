5310da422720279085966d6072610820b200db24
Trang Truong <trangldtruong@gmail.com>
2023-09-26 22:49:15 -0400

    jQuery was added to get items from local storage

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 var saveBtn = document.querySelectorAll('.saveBtn')
 let date = document.getElementById('currentDay')
 let time = document.getElementById('currentTime')
@@ -25,7 +22,7 @@ if (timeBlock < currentTime) {
 });
 
   
-//add an event listener to btn
+//add an event listener to btn to save list items in local storage
 $(".saveBtn").click (function (event) {
   event.preventDefault()
   let value = $(this).siblings('.description').val();
@@ -36,25 +33,26 @@ $(".saveBtn").click (function (event) {
 
 })
 
-
+//using jQuerry to get items from Local storage
 var hourNine = localStorage.getItem('hour-9');
 $("#hour-9 .description").val(hourNine)
-//$(function )
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+var hourTen = localStorage.getItem('hour-10');
+$("#hour-10 .description").val(hourTen)
+var hourEleven = localStorage.getItem('hour-11');
+$("#hour-11 .description").val(hourEleven)
+var hourTwelve = localStorage.getItem('hour-12');
+$("#hour-12 .description").val(hourTwelve)
+var hourOne = localStorage.getItem('hour-1');
+$("#hour-1 .description").val(hourOne)
+var hourTwo = localStorage.getItem('hour-2');
+$("#hour-2 .description").val(hourTwo)
+var hourThree = localStorage.getItem('hour-3');
+$("#hour-3 .description").val(hourThree)
+var hourFour = localStorage.getItem('hour-4');
+$("#hour-4 .description").val(hourFour)
+var hourFive = localStorage.getItem('hour-5');
+$("#hour-5 .description").val(hourFive)
+
   
   
    
